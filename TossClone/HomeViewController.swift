78f759852c2670d9caf3bc581c0c1c7eeb8a96bd
namgeon1106 <namgeon1106@naver.com>
2022-07-10 17:40:19 +0900

    feat: logoButton 추가

--- TossClone/HomeViewController.swift
+++ TossClone/HomeViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let logoButton: UIButton = {
+        let logoImage = UIImage(named: "Logo")
+        let button = UIButton()
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(logoImage, for: .normal)
+
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 123),
+            button.heightAnchor.constraint(equalToConstant: 44)
+        ])
+
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
