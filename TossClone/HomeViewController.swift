2af0b4dc1cb21876a3b68acfa551ad0a2c4232e4
namgeon1106 <namgeon1106@naver.com>
2022-07-10 17:19:38 +0900

    feat: HomeViewController 추가

---
+++ TossClone/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  TossClone
+//
+//  Created by 김남건 on 2022/07/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+    }
+}
+
+extension HomeViewController {
+    private func layout() {
+
+    }
+}
