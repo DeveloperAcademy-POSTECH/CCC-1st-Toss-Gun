94c88c37da1f565067c0cf8c48c8089a907bb82d
namgeon1106 <namgeon1106@naver.com>
2022-07-10 20:11:19 +0900

    feat: 배경색 적용

--- TossClone/HomeViewController.swift
+++ TossClone/HomeViewController.swift
@@ -45,6 +45,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGray5
         layout()
     }
 }
