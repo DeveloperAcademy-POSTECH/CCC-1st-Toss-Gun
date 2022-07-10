e1440df83a567a45a5ef37f9be0e1b94550bc2fd
namgeon1106 <namgeon1106@naver.com>
2022-07-10 17:57:03 +0900

    feat: 채팅 버튼, 알림 버튼 추가

--- TossClone/HomeViewController.swift
+++ TossClone/HomeViewController.swift
@@ -23,6 +23,30 @@ class HomeViewController: UIViewController {
         return button
     }()
 
+    let chatButton: UIButton = {
+        let chatImageConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let chatImage = UIImage(systemName: "message.fill", withConfiguration: chatImageConfig)
+        let button = UIButton()
+        button.tintColor = .systemGray
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(chatImage, for: .normal)
+
+        return button
+    }()
+
+    let notificationButton: UIButton = {
+        let notificationImageConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let notificationImage = UIImage(systemName: "bell.fill", withConfiguration: notificationImageConfig)
+        let button = UIButton()
+        button.tintColor = .systemGray
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(notificationImage, for: .normal)
+
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
